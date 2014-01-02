d3356b7fc33f1a936e612b29d1cebfb76e973b91
dahal <pdahal@me.com>
2014-01-02 14:33:26 -0600

    rails_apps_composer: controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 YouCodeio::Application.routes.draw do
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
